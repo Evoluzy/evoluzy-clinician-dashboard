824c24d4890c5692e9c7cbb90a93ad53db581a2b
LUXIANZE <55844389+LUXIANZE@users.noreply.github.com>
2021-01-28 10:48:38 +0800

    Revert "Revert "patient dashboard, fusion chart""

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Clinician Dashboard</h1>
+      <a href="/health-dashboard/1">Demo</a>
+    </div>
+  )
+}
+
+export default Home;
